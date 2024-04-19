27c30ebc20ba1e2aa73b36bc8d3ec910ba1a2bb4
Guilherme <g.satilio@gmail.com>
2024-04-19 19:02:51 -0300

    Add project files.

---
+++ Trab-SalarioDif/Program.cs
@@ -0,0 +1,28 @@
+﻿//5 - faça um programa que leia o valor atual do salário de uma pessoa e o valor com acréscimo e informe a % de acréscimo que foi dada [(maior / menor) x 100 ]
+
+double salario = 0, salariomaior = 0, calculo = 0;
+
+while (salario <= 0)
+{
+    Console.WriteLine("Informe o valor do seu salário atual:");
+    salario = double.Parse(Console.ReadLine());
+}
+
+while ((salariomaior <= 0))
+{
+    Console.WriteLine("Informe o valor do seu salário com valor acrescentado:");
+    salariomaior = double.Parse(Console.ReadLine());
+}
+
+while ((salariomaior <= salario))
+{
+    Console.WriteLine("Informe o valor do seu salário com valor acrescentado (Precisa ser maior que o salário atual):");
+    salariomaior = double.Parse(Console.ReadLine());
+}
+
+calculo = ((salariomaior / salario) * 100);
+
+Console.WriteLine($"O aumento do salário foi de {calculo}%.");
+
+Console.ReadKey();
+
