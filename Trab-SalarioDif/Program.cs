7991e476145e6eaa03b8115bf65a112888639990
Guilherme <g.satilio@gmail.com>
2024-04-21 19:17:59 -0300

    melhoria 1

--- Trab-SalarioDif/Program.cs
+++ Trab-SalarioDif/Program.cs
@@ -1,28 +1,42 @@
 ﻿//5 - faça um programa que leia o valor atual do salário de uma pessoa e o valor com acréscimo e informe a % de acréscimo que foi dada [(maior / menor) x 100 ]
 
 double salario = 0, salariomaior = 0, calculo = 0;
+string sair;
+sair = "N";
 
-while (salario <= 0)
+while (sair != "S" && sair != "s")
 {
-    Console.WriteLine("Informe o valor do seu salário atual:");
-    salario = double.Parse(Console.ReadLine());
+    while (salario <= 0)
+    {
+        Console.WriteLine("Informe o valor do seu salário atual:");
+        salario = double.Parse(Console.ReadLine());
+    }
+
+    while ((salariomaior <= 0))
+    {
+        Console.WriteLine("Informe o valor do seu salário com valor acrescentado:");
+        salariomaior = double.Parse(Console.ReadLine());
+    }
+
+    while ((salariomaior <= salario))
+    {
+        Console.WriteLine("Informe o valor do seu salário com valor acrescentado (Precisa ser maior que o salário atual):");
+        salariomaior = double.Parse(Console.ReadLine());
+    }
+
+    calculo = ((salariomaior / salario) * 100);
+
+    Console.WriteLine($"O aumento do salário foi de {calculo}%.");
+    
+    Console.WriteLine("Digite S para sair do programa ou aperte qualquer tecla para repetir.");
+    sair = Console.ReadLine();
+    if (sair != "S" && sair != "s")
+    {
+        salario = 0;
+        salariomaior = 0;
+        calculo = 0;
+        Console.Clear();
+    }
 }
-
-while ((salariomaior <= 0))
-{
-    Console.WriteLine("Informe o valor do seu salário com valor acrescentado:");
-    salariomaior = double.Parse(Console.ReadLine());
-}
-
-while ((salariomaior <= salario))
-{
-    Console.WriteLine("Informe o valor do seu salário com valor acrescentado (Precisa ser maior que o salário atual):");
-    salariomaior = double.Parse(Console.ReadLine());
-}
-
-calculo = ((salariomaior / salario) * 100);
-
-Console.WriteLine($"O aumento do salário foi de {calculo}%.");
-
 Console.ReadKey();
 
